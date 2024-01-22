b60e20adc0c1ea4c0b5c77a5ad49c42dff1066b6
Guillaume <guillaumebaillieux88@gmail.com>
2024-01-22 16:23:23 +0100

    ex29et30

---
+++ stagiaires/Guillaume/29-exemples-fonctions.php
@@ -0,0 +1,26 @@
+<?php
+
+/* 
+Pour créer une fonction, on utilise le mot clef function 
+Suivi du nom de la fonction (règles de nommage des variables sans le &)
+suivie des (){}
+*/
+
+function helloWorld(){
+    return "Hello World !";
+}
+
+// une fonction n'est exécutée que lorsqu'on l'appelle 
+
+echo helloWorld(); 
+
+function lanceDe(int $faces){
+    $array = array();
+    for($i=1; $i<=$faces;$i++){
+        $array[$i] = $i;
+     
+  }
+  return $array;
+}
+
+var_dump(lanceDe(6));
